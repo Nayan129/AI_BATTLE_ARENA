f781aaa728bedeeaaaed1fede0b3e0894bd985fb
Nayan <nayanbhusari02@gmail.com>
2026-04-03 19:52:15 +0530

    ai solution recommendation done

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
-
+import useGraph from "./services/graph.ai.service.js"
 const app = express();
 
-app.get("/",(req,res)=>{
+app.get("/health",(req,res)=>{
   res.status(200).json({
     status:"ok"
   })
+})
+
+app.post("/use-graph",async (req,res)=>{
+  await useGraph("what is the capital of America?")
 })
 export default app;
